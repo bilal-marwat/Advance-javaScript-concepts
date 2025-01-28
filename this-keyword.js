9be3876aca2cdc80a51f534da383507961ac6e6f
bilal-marwat <bilalmarwat134@gmail.com>
2025-01-28 20:33:06 +0500

    add more example

--- this-keyword.js
+++ this-keyword.js
@@ -1,3 +1,4 @@
+"use strict";
 //defination this keyword
 
 //this keyword JavaScript ka ek powerful concept hai jo current execution context ko refer karta hai. Ye different situations me different values le sakta hai, depending on ki code kis tarah se likha gaya hai aur kis tarah se call kiya gaya hai.
@@ -61,3 +62,46 @@ strcitMode();
 //     console.log(this.userName);
 // }
 // greetArrow();
+//========================================================================================================
+
+// This keyword work in Global Scope
+
+// var student = "Bilal Marwat";
+// console.log(window.student); // this keyword in global scope always represents gobal object in browser window object
+
+
+// Q: if interviewer ask you what is the value of this inside global scope??
+//Ans: it is global object that can be different it can be window it can be global it can be something else depending on the environment you are working in where are you running javascript code
+
+
+
+//NOTED: this keyword work different under strict mode and non strict mode 
+
+
+//this inside a function
+function x(){
+  console.log(this);  // results global  object window object 
+  // if use strict mode then it will be undefined if strict mode is enabled then it will be global object window object
+}
+x();
+
+// this inside non-strict mode - (this substitution)
+/*
+if the value of this keyword is undefined or null this keyword will be replaced with global object only in non strict mode 
+*/
+
+
+
+
+//this inside a object method
+
+//inside a function is called as method
+
+const user = {
+    name : "Muhammad Bilal",
+    age : 20,
+    greet : function(){
+        console.log(this.name); // this keyword inside object method will be object itself
+    }
+}
+user.greet();
